75259946cd722dcfffa68de59ae366d9b1c5e06b
JessicaLaurenB <mathlove1255@gmail.com>
2016-08-20 23:39:56 -0400

    employee info

---
+++ app/src/main/java/com/onesol/pointofsale/employeePopUp.java
@@ -0,0 +1,22 @@
+package com.onesol.pointofsale;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class employeePopUp extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_pop_up);
+        setTitle("Employee Info");
+    }
+
+
+
+}
